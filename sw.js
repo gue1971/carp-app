857c0fc23c743d26986a115812121cd4ee4bcac9
gue1971 <gue1971@gmail.com>
2026-03-21 10:04:20 +0900

    Add PWA support with app icon

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'carp-app-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './players-data.js',
+  './npb-data.js',
+  './manifest.webmanifest',
+  './carp-logo.svg',
+  './boy.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request).then((networkResponse) => {
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type === 'error') {
+          return networkResponse;
+        }
+
+        const requestUrl = new URL(event.request.url);
+        if (requestUrl.origin === self.location.origin) {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        }
+
+        return networkResponse;
+      });
+    })
+  );
+});
